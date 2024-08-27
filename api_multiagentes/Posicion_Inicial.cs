48bc509441518b824722a9ce6b0f6845218e8c0f
LuzPaty <luzpatyhr@gmail.com>
2024-08-27 16:56:19 -0600

    se me habia olvidado actualizar aqui los otros 2 scripts

--- api_multiagentes/Posicion_Inicial.cs
+++ api_multiagentes/Posicion_Inicial.cs
@@ -1,26 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Posicion_Inicial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SendPositions(List<GameObject> objects)
     {
-     StartCoroutine(GetRequest("http://127.0.0.1:5000/"));   
+        if (objects == null || objects.Count == 0)
+        {
+            Debug.LogError("Objects list is empty. Nothing to send.");
+            return;
+        }
+
+        string json = "{ \"positions\": [";
+        for (int i = 0; i < objects.Count; i++)
+        {
+            Vector3 pos = objects[i].transform.position;
+            string name = objects[i].name;
+
+            json += "{ \"x\": " + pos.x + ", \"z\": " + pos.z + ", \"name\": \"" + name + "\"}";
+            if (i < objects.Count - 1)
+            {
+                json += ",";
+            }
+        }
+        json += "]}";
+
+        Debug.Log("Sending JSON: " + json);
+
+        StartCoroutine(PostRequest("http://127.0.0.1:5000/", json));
     }
 
-    IEnumerator GetRequest(string uri){
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri)){
-            yield return webRequest.SendWebRequest();
-            if (webRequest.result != UnityWebRequest.Result.Success){
-                Debug.Log(webRequest.error);
-            } else {
-                // Show results as text
-                Debug.Log(webRequest.downloadHandler.text);
-                // Or retrieve results as binary data
-                byte[] results = webRequest.downloadHandler.data;
-            }
+    IEnumerator PostRequest(string uri, string json)
+    {
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(json);
+        UnityWebRequest webRequest = new UnityWebRequest(uri, "POST");
+        webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        webRequest.downloadHandler = new DownloadHandlerBuffer();
+        webRequest.SetRequestHeader("Content-Type", "application/json");
+
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.result != UnityWebRequest.Result.Success)
+        {
+            Debug.Log(webRequest.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete! Response: " + webRequest.downloadHandler.text);
+        }
     }
-}
 }
--- api_multiagentes/RandomSpawner.cs
+++ api_multiagentes/RandomSpawner.cs
@@ -6,14 +6,9 @@ public class RandomSpawner : MonoBehaviour
 {
     public GameObject cubePrefab;
     public GameObject robotPrefab;
-
     public CalculateFloorBounds boundsCalculator;
+    public Posicion_Inicial positionSender; // Reference to the Posicion_Inicial script
 
-    // Arrays (Lists) to store the positions
-    private List<Vector3> cubePositions = new List<Vector3>();
-    private List<Vector3> robotPositions = new List<Vector3>();
-
-    // Lists to store the instances
     private List<GameObject> cubeInstances = new List<GameObject>();
     private List<GameObject> robotInstances = new List<GameObject>();
 
@@ -24,79 +19,38 @@ public class RandomSpawner : MonoBehaviour
             Vector3 minBounds = boundsCalculator.minBounds;
             Vector3 maxBounds = boundsCalculator.maxBounds;
 
-            // Clear previous positions and instances
-            cubePositions.Clear();
-            robotPositions.Clear();
             cubeInstances.Clear();
             robotInstances.Clear();
 
-            // Spawn 10 valid cubes
             for (int i = 0; i < 10; i++)
             {
                 Vector3 randomSpawnPosition = GetValidSpawnPosition(minBounds, maxBounds, 0f);
-                Debug.Log("Spawning cube at: " + randomSpawnPosition);
                 GameObject cubeInstance = Instantiate(cubePrefab, randomSpawnPosition, Quaternion.identity);
-
-                // Assign a unique name to each cube
-                cubeInstance.name = "Cube" + (i + 1);
-
-                // Save cube position and instance, and debug print
-                cubePositions.Add(randomSpawnPosition);
+                cubeInstance.name = "Cube" + (i + 1);  // Name includes index
                 cubeInstances.Add(cubeInstance);
-                Debug.Log("Saved Cube Position: " + randomSpawnPosition);
             }
 
-            // Spawn 5 valid robots
             for (int i = 0; i < 5; i++)
             {
                 Vector3 randomSpawnPosition = GetValidSpawnPosition(minBounds, maxBounds, 0f);
                 GameObject spawnedRobot = Instantiate(robotPrefab, randomSpawnPosition, Quaternion.identity);
-
-                // Assign a unique name to each robot
-                spawnedRobot.name = "Robot" + (i + 1);
-
-                Rigidbody rb = spawnedRobot.GetComponent<Rigidbody>();
-                Debug.Log($"Spawning robot at: {randomSpawnPosition}");
-
-                // Save robot position and instance, and debug print
-                robotPositions.Add(randomSpawnPosition);
+                spawnedRobot.name = "Robot" + (i + 1);  // Name includes index
                 robotInstances.Add(spawnedRobot);
-                Debug.Log("Saved Robot Position: " + randomSpawnPosition);
             }
 
-            // Debug log final vectors for cubes
-            Debug.Log("Final Cube Positions:");
-            foreach (Vector3 pos in cubePositions)
-            {
-                Debug.Log(pos);
-            }
+            // Combine the lists if you want to send all objects together
+            List<GameObject> allObjects = new List<GameObject>();
+            allObjects.AddRange(cubeInstances);
+            allObjects.AddRange(robotInstances);
 
-            // Debug log final vectors for robots
-            Debug.Log("Final Robot Positions:");
-            foreach (Vector3 pos in robotPositions)
-            {
-                Debug.Log(pos);
-            }
-
-            // Debug log instances for cubes
-            Debug.Log("Final Cube Instances:");
-            foreach (GameObject cube in cubeInstances)
-            {
-                Debug.Log(cube.name + " at position: " + cube.transform.position);
-            }
-
-            // Debug log instances for robots
-            Debug.Log("Final Robot Instances:");
-            foreach (GameObject robot in robotInstances)
-            {
-                Debug.Log(robot.name + " at position: " + robot.transform.position);
-            }
+            // Send all object positions and names
+            positionSender.SendPositions(allObjects);
         }
     }
 
     Vector3 GetValidSpawnPosition(Vector3 minBounds, Vector3 maxBounds, float height)
     {
-        while (true) // Loop until a valid position is found
+        while (true)
         {
             Vector3 randomPosition = new Vector3(
                 Random.Range(minBounds.x, maxBounds.x),
@@ -104,7 +58,6 @@ public class RandomSpawner : MonoBehaviour
                 Random.Range(minBounds.z, maxBounds.z)
             );
 
-            // Raycast downward to check if there's a shelf (Estante) below
             RaycastHit hit;
             if (Physics.Raycast(randomPosition, Vector3.down, out hit, Mathf.Infinity))
             {
@@ -112,45 +65,18 @@ public class RandomSpawner : MonoBehaviour
 
                 if (hit.collider.CompareTag("Estante"))
                 {
-                    // If the raycast hits an object with the tag "Estante", skip this position
                     continue;
                 }
                 else
                 {
-                    // If no "Estante" is detected below, return this as a valid spawn position
                     Vector3 validPosition = hit.point + Vector3.up * 0.5f;
-                    Debug.Log("Valid position found: " + validPosition);
-                    return validPosition; // Adjust to just above the hit point
+                    return validPosition;
                 }
             }
             else
             {
-                // If the raycast hits nothing (i.e., open ground), return the position
-                Debug.Log("Open ground position found: " + randomPosition);
                 return randomPosition;
             }
         }
-    }
-
-    // Methods to access the stored positions
-    public List<Vector3> GetCubePositions()
-    {
-        return new List<Vector3>(cubePositions);
-    }
-
-    public List<Vector3> GetRobotPositions()
-    {
-        return new List<Vector3>(robotPositions);
-    }
-
-    // Methods to access the stored instances
-    public List<GameObject> GetCubeInstances()
-    {
-        return new List<GameObject>(cubeInstances);
-    }
-
-    public List<GameObject> GetRobotInstances()
-    {
-        return new List<GameObject>(robotInstances);
     }
 }
