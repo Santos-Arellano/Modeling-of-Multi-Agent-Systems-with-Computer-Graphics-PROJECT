6ce280a0c1b53bfbf99418b7dd1e93524d341703
LuzPaty <luzpatyhr@gmail.com>
2024-08-27 19:40:22 -0600

    ruta receive_position

--- api_multiagentes/app.py
+++ api_multiagentes/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/receive_position', methods=['POST'])
 def receive_positions():
     data = request.get_json()
     print("Received data:", data)
